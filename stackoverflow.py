3aedb0449df61fb6e071a9d6b74792cd084da164
Neuromancer93 <itrejonumber1@gmail.com>
2020-02-29 16:38:46 -0800

    feat: initial scraping results of stackoverflow

--- stackoverflow.py
+++ stackoverflow.py
@@ -1,10 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 res = requests.get("https://stackoverflow.com/questions")
 
 soup = BeautifulSoup(res.text, "html.parser")
 
+questions_data = {
+    'questions': []
+}
+
 questions = soup.select(".question-summary")
 
-print(questions[0].select_one('.question-hyperlink').getText())
+for question in questions:
+    q = question.select_one('.question-hyperlink').getText()
+    vote_count = question.select_one('.vote-count-post').getText()
+    views = question.select_one('.views').attrs['title']
+    questions_data['questions'].append({
+        'question': q,
+        'views': views,
+        'vote_count': vote_count
+    })
+
+json_data = json.dumps(questions_data)
+
+print(json_data)
