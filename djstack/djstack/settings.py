5cce3dbbb248bdecbfcef187b26c1404f3807ffa
Neuromancer93 <itrejonumber1@gmail.com>
2020-03-14 11:47:47 -0700

    feat: created django rest api from beautiful soup scraper. Scraping stackoverflow questions

--- djstack/djstack/settings.py
+++ djstack/djstack/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y6a3o_(5#==_)25-v)-lr9gpv42(3d0df!ia%9pg07-_#=sxtt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
